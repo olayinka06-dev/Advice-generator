967147899dba8168d2d18130a04d9ee9b01a1c53
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-11 13:34:07 -0700

    creating the speech button

--- src/components/entities/Entity.jsx
+++ src/components/entities/Entity.jsx
@@ -5,7 +5,14 @@ import { FaPauseCircle } from "react-icons/fa";
 import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
 import { useAdviceContext } from "../provider/Context";
 
-
+export const SpeechButton = () => {
+    const { isSpeaking, handleSpeakJoke, handleStopJoke } = useAdviceContext();
+    return (
+      <button onClick={isSpeaking ? handleStopJoke : handleSpeakJoke}>
+        {isSpeaking ? <FaPauseCircle /> : <BsFillPlayCircleFill />}
+      </button>
+    );
+};
 export const PreLoader = () => {
   return (
     <div className="w-[50px] h-[50px] rounded-full border-solid border-t-[red] border-[5px] animation"></div>
