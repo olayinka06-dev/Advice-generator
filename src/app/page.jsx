56aa6cc157a183d22e8ee9f248ccbc8125b51e8b
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-12 04:29:40 -0700

    making overall adjustment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,11 +8,19 @@ export default function Home() {
   return (
     <section
       className={`min-h-screen px-5 flex flex-col justify-center items-center ${
-        isToggled ? "light-mode bg-[var(--primary-color)]" : "dark-mode bg-[var(--primary-color)]"
+        isToggled
+          ? "light-mode bg-[var(--primary-color)]"
+          : "dark-mode bg-[var(--primary-color)]"
       } w-full `}
     >
       <ThemeButton />
       <JokesGenerator />
+      <div className="text-[var(--text-color)] absolute text-center bottom-1">
+        <span className="">
+          Built with Love by <span className="text-[red]">Olayinka_Dev</span>
+        </span> <br />
+        <span>Follow and give it a star on </span><button>Github</button>
+      </div>
     </section>
   );
 }
--- src/components/advice/JokesGenerator.jsx
+++ src/components/advice/JokesGenerator.jsx
@@ -6,43 +6,46 @@ import { CopyButton, GeneratedJoke, PreLoader, SpeechButton } from "../entities/
 const JokesGenerator = () => {
   const { jokeQoute, error, isLoading, } = useAdviceContext();
   return (
-    <div className="bg-[var(--secondary-color)] flex flex-col gap-3 relative px-6 max-w-[500px] w-full py-3 rounded-2xl min-h-[48vh]">
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-between items-center">
+    <section>
+      <h1 className="text-center text-[var(--text-color)] text-[30px]">The Advice Generator</h1>
+      <div className="bg-[var(--secondary-color)] shadow flex flex-col gap-3 relative px-6 max-w-[500px] w-full py-3 rounded-2xl min-h-[48vh]">
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center">
             <SpeechButton />
             <CopyButton />
-        </div>
-        {isLoading ? (
-          <PreLoader />
-        ) : (
-          <div className="flex flex-col gap-1">
-            <div className="text-center">
-              {jokeQoute && (
-                <h3 className="text-[hsl(150,100%,66%)]">
-                  Advice # {jokeQoute.slip.id}
-                </h3>
-              )}
-              {error && <p className="text-[red]">{error}</p>}
-            </div>
-            <div className="text-center">
-              {jokeQoute && (
-                <span className="text-[var(--text-color)] text-[20px]">
-                  &quot;{jokeQoute.slip.advice}&quot;
-                </span>
-              )}
-            </div>
           </div>
-        )}
-      </div>
-      <div className="">
-        <img className="w-full" src="/images/pattern-divider-mobile.svg" alt="border" />
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="absolute bottom-[-15px]">
+          {isLoading ? (
+            <PreLoader />
+          ) : (
+            <div className="flex flex-col gap-1">
+              <div className="text-center">
+                {jokeQoute && (
+                  <h3 className="text-[hsl(150,100%,66%)]">
+                    Advice # {jokeQoute.slip.id}
+                  </h3>
+                )}
+                {error && <p className="text-[red]">{error}</p>}
+              </div>
+              <div className="text-center">
+                {jokeQoute && (
+                  <span className="text-[hsl(193,38%,86%)] text-[20px]">
+                    &quot;{jokeQoute.slip.advice}&quot;
+                  </span>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="">
+          <img className="w-full" src="/images/pattern-divider-mobile.svg" alt="border" />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="absolute bottom-[-15px]">
             <GeneratedJoke/>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
