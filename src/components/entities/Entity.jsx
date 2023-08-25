fd3c51eb50d070256bba3bbec777753d3c869b17
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-25 07:41:07 -0700

    Ajusting the Entity js

--- src/components/entities/Entity.jsx
+++ src/components/entities/Entity.jsx
@@ -6,38 +6,39 @@ import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
 import { useAdviceContext } from "../provider/Context";
 
 export const CopyButton = () => {
-  const { isCopy, handleCopyJoke, validateButton } = useAdviceContext();
+  const { allData } = useAdviceContext();
 
   return (
     <button
       className="py-[10px] rounded-2xl text-[var(--btn-color)] hover:bg-transparent px-5 bg-[hsl(150,100%,66%)] border transition duration-400 ease-linear border-transparent border-solid hover:text-[hsl(193,38%,86%)] hover:border hover:border-solid hover:border-[hsl(150,100%,66%)]"
-      onClick={handleCopyJoke}
-      disabled={validateButton ? false : true}
+      onClick={allData.handleCopyJoke}
+      disabled={allData.validateButton ? false : true}
     >
-      {isCopy ? "Copied!" : "Copy"}
+      {allData.isCopy ? "Copied!" : "Copy"}
     </button>
   );
 };
 
 export const SpeechButton = () => {
-  const { isSpeaking, handleSpeakJoke, handleStopJoke, validateButton } =
-    useAdviceContext();
+  const { allData } = useAdviceContext();
   return (
     <button
       className="py-[10px] rounded-2xl hover:bg-transparent px-5 bg-[hsl(150,100%,66%)] border transition duration-400 ease-linear border-transparent text-[var(--btn-color)] border-solid hover:text-[hsl(193,38%,86%)] hover:border hover:border-solid hover:border-[hsl(150,100%,66%)] text-[19px]"
-      onClick={isSpeaking ? handleStopJoke : handleSpeakJoke}
-      disabled={validateButton ? false : true}
+      onClick={
+        allData.isSpeaking ? allData.handleStopJoke : allData.handleSpeakJoke
+      }
+      disabled={allData.validateButton ? false : true}
     >
-      {isSpeaking ? <FaPauseCircle /> : <BsFillPlayCircleFill />}
+      {allData.isSpeaking ? <FaPauseCircle /> : <BsFillPlayCircleFill />}
     </button>
   );
 };
 
 export const GeneratedJoke = () => {
-  const { handleGenerateJoke } = useAdviceContext();
+  const { allData } = useAdviceContext();
   return (
     <button
-      onClick={handleGenerateJoke}
+      onClick={allData.handleGenerateJoke}
       className="bg-[hsl(150,100%,66%)] blur-shadow p-4 rounded-full"
     >
       <BsDice5Fill />
@@ -46,14 +47,14 @@ export const GeneratedJoke = () => {
 };
 
 export const ThemeButton = () => {
-  const { handleSwitchTheme, isToggled } = useAdviceContext();
+  const { allData } = useAdviceContext();
   return (
     <div className=" absolute top-0 right-5">
       <h2 className="text-white">Theme</h2>
       <div className="flex items-center flex-row gap-2">
         <span
           className={`text-[20px] ${
-            isToggled ? "text-[hsl(193,23%,77%)]" : "text-white"
+            allData.isToggled ? "text-[hsl(193,23%,77%)]" : "text-white"
           }`}
         >
           <BsFillMoonStarsFill />
@@ -63,24 +64,24 @@ export const ThemeButton = () => {
             type="checkbox"
             id="toggleButton"
             className="sr-only"
-            checked={isToggled}
-            onChange={handleSwitchTheme}
+            checked={allData.isToggled}
+            onChange={allData.handleSwitchTheme}
           />
           <div
             className={`w-12 h-6 ${
-              isToggled ? "bg-[rgb(3,40,91)]" : "bg-[rgb(3,40,91)]"
+              allData.isToggled ? "bg-[rgb(3,40,91)]" : "bg-[rgb(3,40,91)]"
             } rounded-full transition`}
           >
             <div
               className={`absolute w-4 h-4 bg-white rounded-full top-1 left-1 transition-transform transform ${
-                isToggled ? "translate-x-6" : "translate-x-0"
+                allData.isToggled ? "translate-x-6" : "translate-x-0"
               }`}
             />
           </div>
         </label>
         <span
           className={`text-[20px] ${
-            isToggled ? "text-white" : "text-[hsl(193,23%,77%)]"
+            allData.isToggled ? "text-white" : "text-[hsl(193,23%,77%)]"
           }`}
         >
           <BsSun />
